5ca4e63342b26a85fa12f57e30f8320888f239a8
tamashii <terrocraft3223@gmail.com>
2026-01-10 11:29:53 +0100

    Fix little navbar issue.

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -2,13 +2,13 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
-    const { pathname, hash } = useLocation();
+    const { pathname, hash, key } = useLocation();
 
     useEffect(() => {
         if (!hash) {
             window.scrollTo(0, 0);
         }
-    }, [pathname, hash]);
+    }, [pathname, hash, key]);
 
     return null;
 }
